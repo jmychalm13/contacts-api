6c8f37ac6615d9efbaec1ab5d43b8952f36d8181
“Jessica <jmychalm13@gmail.com>
2023-07-03 20:45:45 -0500

    model and database

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,10 +1,2 @@
-class Contact
-  attr_accessor :first_name, :last_name, :email, :phone_number
-
-  def initialize(options_hash)
-    @first_name = options_hash[:first_name]
-    @last_name = options_hash[:last_name]
-    @email = options_hash[:email]
-    @phone_number = options_hash[:phone_number]
-  end
+class Contact < ApplicationRecord
 end
